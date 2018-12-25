53eb2a67af026ca434a824b74c6f8418b618b947
vladislav <ve@incsub.com>
2018-12-25 09:37:10 +0100

    Add hardcoded path ajax actions discovery

--- test.py
+++ test.py
@@ -2,7 +2,29 @@ from wpfuzz.request import ajax
 from wpfuzz import fuzzer
 
 
+import glob
+ajax_calls = []
+for phpfile in glob.glob("/home/ve/Env/wpd/projects/plugins/shipper/**/*.php", recursive=True):
+    with open(phpfile, 'r') as f:
+        for line in f.readlines():
+            if line.find('wp_ajax') != -1:
+                start = line.find('wp_ajax_')
+                end = line.find("'", start)
+                ajax_call = ''
+                if end == -1:
+                    end = line.find('"', start)
+                if end > 0 and end > start:
+                    ajax_call = line[start:end]
+                else:
+                    continue
+
+                if ajax_call[-1] != '_' and -1 == ajax_call.find("%s"):
+                    ajax_calls.append(
+                        ajax_call.replace("wp_ajax_", "", 1).replace("norpiv_", "", 1)
+                    )
+
+
 x = ajax.Caller('http://singlewp.test', 'bog', 'bog')
-#f = Fuzzer(x, 'shipper_download_log')
-f = fuzzer.Fuzzer(x, 'shipper_modal_closed')
-f.fuzz().report()
+for action in ajax_calls:
+    f = fuzzer.Fuzzer(x, action)
+    f.fuzz().report()
