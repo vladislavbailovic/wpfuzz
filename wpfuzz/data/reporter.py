e17152f989bdad494e86ed41ef744c03cff02e5e
vladislav <ve@incsub.com>
2018-12-25 09:37:25 +0100

    Only print interesting results

--- wpfuzz/data/reporter.py
+++ wpfuzz/data/reporter.py
@@ -1,3 +1,5 @@
+import json
+
 class Reporter:
 
     identifier = None
@@ -22,6 +24,15 @@ class Reporter:
         print("Report for {}".format(self.identifier))
         for r in self.results:
             status = r.get('response').status_code
+            if 200 != status:
+                continue
+            try:
+                json_resp = json.loads(r.get('response').text)
+                if "success" in json_resp and not json_resp.get("success"):
+                    continue
+            except:
+                pass
+
             auth = "Authenticated" if r.get('auth') else "Visitor"
             printable = {trunc(key): trunc(val) for (key,val) in r.get('original').items()}
             print("{} {} [{}]".format(auth, r.get('response').request.method, status))
