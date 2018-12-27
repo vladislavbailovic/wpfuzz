3551be7c1f12f7e85441d69903030fea7a0d2879
vladislav <ve@incsub.com>
2018-12-27 06:23:53 +0100

    Fix lazy fuzz data class instantiation

--- wpfuzz/fuzzer.py
+++ wpfuzz/fuzzer.py
@@ -19,10 +19,10 @@ class Fuzzer:
 
     def fuzz(self, iters=5):
         report = reporter.ReporterProxy(self.action)
+        fuzzers = [f({"action": self.action}) for f in self.fuzzers]
 
         for idx in range(1, iters):
-            for fuzzdata_class in self.fuzzers:
-                fuzzdata = fuzzdata_class({"action": self.action})
+            for fuzzdata in fuzzers:
                 fuzz = fuzzdata.get_data()
                 for result in self.caller.ajax_call(fuzz):
                     report.add_result(*result, original=fuzz)
